85752099e0780c1d1cfb10df98f0e7d40c9d492a
Renan Lopes Ferreira <relferreira@gmail.com>
2016-10-29 22:28:35 -0400

    Inclusion of Butterknife

--- app/src/main/java/com/relferreira/gitnotify/main/MainActivity.java
+++ app/src/main/java/com/relferreira/gitnotify/main/MainActivity.java
@@ -1,18 +1,32 @@
 package com.relferreira.gitnotify.main;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.relferreira.gitnotify.R;
 import com.relferreira.gitnotify.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity<MainPresenter> implements MainView {
+
+    @BindView(R.id.tabs) TabLayout tabs;
+    @BindView(R.id.viewpager) ViewPager viewPager;
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
 
         presenter.loadToastMsg();
     }
