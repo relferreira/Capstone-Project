96eb22c5893b2ee542d52ffb281c4795de7bd9bf
Renan Lopes Ferreira <relferreira@gmail.com>
2017-01-15 18:29:11 -0500

    Remove of gradle properties

---
+++ app/src/main/java/com/relferreira/gitnotify/model/Login.java
@@ -0,0 +1,7 @@
+package com.relferreira.gitnotify.model;
+
+/**
+ * Created by relferreira on 1/15/17.
+ */
+public class Login {
+}
---
+++ app/src/main/java/com/relferreira/gitnotify/util/AuthErrorHelper.java
@@ -0,0 +1,7 @@
+package com.relferreira.gitnotify.util;
+
+/**
+ * Created by relferreira on 1/15/17.
+ */
+public class AuthErrorHelper {
+}
